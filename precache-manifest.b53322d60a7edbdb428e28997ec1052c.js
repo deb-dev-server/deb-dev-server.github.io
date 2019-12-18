327cc3184f615f58ed6835c4ee9f1c5c5b793325
deb-dev-server <52189322+deb-dev-server@users.noreply.github.com>
2019-12-18 14:58:17 +0600

    Add files via upload

---
+++ precache-manifest.b53322d60a7edbdb428e28997ec1052c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "962a499e74e64441c281",
+    "url": "/css/app.c413670f.css"
+  },
+  {
+    "revision": "c13b12498d18d66950ef",
+    "url": "/css/chunk-vendors.db7de621.css"
+  },
+  {
+    "revision": "78c45838ba3a210e2c9ecf0190192990",
+    "url": "/index.html"
+  },
+  {
+    "revision": "962a499e74e64441c281",
+    "url": "/js/app.9a3aeec8.js"
+  },
+  {
+    "revision": "c13b12498d18d66950ef",
+    "url": "/js/chunk-vendors.39df600c.js"
+  },
+  {
+    "revision": "3e6a5e9c99ef6e8c29a97292d544a90b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
